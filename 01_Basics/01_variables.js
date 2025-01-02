c6a729a1dd17c31b1406f434843f233aab1c303d
NupurJGit <nupurjoshi811@gmail.com>
2025-01-02 16:10:56 +0000

    Changes for variables and folder structure

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144852;
+let accountEmail = "abcder@gmail.com";
+var accountPassword = "7896";
+accountCity = "Banglore"; //----> this is NOT RECOMMENDED syntax to define a variable. Variable should ALWAYS be defined with 'let' OR 'var' keyword
+
+// accountId = 2;
+/*
+  this is not allowed as accountId is constant
+*/
+console.table([accountId, accountEmail,accountPassword,accountCity])
+
+let accountState;
+
+accountEmail = "xyzeriowjt@yopmail.com"
+accountPassword = "iuhfw";
+accountCity = "Nagpur";
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
