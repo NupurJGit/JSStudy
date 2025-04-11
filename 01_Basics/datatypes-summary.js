d250346bfaf2d8ecbd2d5fd23a8422675140f2c2
NupurJGit <nupurjoshi811@gmail.com>
2025-04-11 10:16:19 +0530

    additional notes for datatypes summary

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -1,13 +1,13 @@
 // Javascript is a dynamically typed language.
 
 //Primitive
-//7 types - String, Number, Boolean, Undefined, Null, Symbol, BigInt
+//7 types - String, Number, Boolean, Undefined, Null, Symbo  l, BigInt
 
 let symbolEx = Symbol("14587");
 
 let symbolEx2 = Symbol("14587");
 
-console.log(symbolEx == symbolEx2);
+console.log(symbolEx == symbolEx2); // this returns false because each symbol will give a different unique value. So the equal comparison will always return false.
 
 
 //Non Primitive -> also called Reference type -> meaning their reference is allocated in memory
@@ -28,4 +28,21 @@ console.log(heros +" is of type "+typeof(heros));
 console.log(obj1 +" is of type "+typeof(obj1));
 console.log(fun1  +" is of type "+typeof(fun1));
 
+
+// the typeof ran on null will return Object.
+
+//Return type of variables in JavaScript
+//1) Primitive Datatypes
+  //     Number => number
+    //   String  => string
+      // Boolean  => boolean
+      // null  => object
+      // undefined  =>  undefined
+      // Symbol  =>  symbol
+      // BigInt  =>  bigint
+
+//2) Non-primitive Datatypes
+  //     Arrays  =>  object
+   //    Function  =>  function
+   //    Object  =>  object
 
